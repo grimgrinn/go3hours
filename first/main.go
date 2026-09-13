3787a33eaea4340fa9c2f7537544c427f99bbb79
grimgrinn <grimgrinn@gmail.com>
2026-09-13 06:17:06 +0300

    practice first

--- first/main.go
+++ first/main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	fmt.Printf("film %s rating %.3f year %d", "interstallar", 8.655, 2014)
 }
---
+++ first/practice/film.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Print("Фильм: Интерстеллар\n")
+	fmt.Println("Фантастика, 2014")
+	fmt.Printf("Рейтинг: %.1f | %d мин | Режиссер: %s ", 8.6, 169, "Кристофер Нолан")
+
+}
